98d5ef0ebe1eb91926a9621776ca72772f6ff1c3
samatheusleite <sa.matheus.leite@gmail.com>
2020-05-15 04:59:22 +0000

    create a lowercase dictionary

--- main.c
+++ main.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
 #include <string.h>
 
 void copy_content(FILE *file1, FILE *file2){
@@ -6,54 +8,30 @@ void copy_content(FILE *file1, FILE *file2){
   int i=0;
   printf("escrevendo...\n");
   while (fgets(Line, 50, file1) != NULL){
-    //1. aceitar apenas palavras com mais de 4 letras
-    if (strlen(Line)>4){
-      //2. verificar se primeira letra lida é igual a da palavra anterior
-      if (Line[0] == Buffer[0]){
-        //3. verificar se segunda letra lida é igual
-        if (Line[1] == Buffer[1]) {
-          //4. verificar se a terceira letra lida é igual
-          if (Line[2] == Buffer[2]){
-            //5. se a quarta lida for diferente, escreve a palavra e muda a última letra do buffer
-            if (Line[3] != Buffer[3]){
-              fputs(Line, file2);
-              Buffer[3] = Line[3];
-            } //5. senão, ignora a palavra
-          } //4. senão, escreve a palavra e muda as duas últimas letras do buffer
-          else{
-              fputs(Line, file2);
-              Buffer[2] = Line[2];
-              Buffer[3] = Line[3];
-            }
-        } //3. senão, escreve a palavra e muda as 3 últimas letras do buffer
-        else {
-          fputs(Line, file2);
-          Buffer[1] = Line[1];
-          Buffer[2] = Line[2];
-          Buffer[3] = Line[3];
-        }
-      } //2. senão, escreve a palavra e muda o buffer
-      else {
-        fputs(Line, file2);
-        Buffer[0] = Line[0];
-        Buffer[1] = Line[1];
-        Buffer[2] = Line[2];
-        Buffer[3] = Line[3];
-      }
-    } //1. senão, ignora a palavra
+    int j=0;
+    while (Line[j]!='\n'){
+      Line[j] = tolower(Line[j]);
+      j++;
+    }
+    fputs(Line, file2);
   }
   printf("finalizado!");
+  return;
 }
- 
-int main() {
-  FILE *dict_unix, *dict_br;
 
-  dict_unix = fopen("unix-br.txt", "r");
-  if (dict_unix == NULL){
-    printf("Problema na leitura do dicionario unix \n");
-    return 0;
-  }
+void error_reading_file(FILE *file){
+  if (file == NULL){
+      printf("Problema na leitura do dicionario\n");
+      exit(0);
+    }
+}
+
+int main(){
+  FILE *dict_unix, *dict_br;
   
+  dict_unix = fopen("unix-br.txt", "r");
+  error_reading_file(dict_unix);
+
   dict_br = fopen("dictionary.txt", "w");
 
   copy_content(dict_unix, dict_br);
